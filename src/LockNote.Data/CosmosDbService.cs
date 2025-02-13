c8d90d34648f87288f5b98a99a9dd846e7be9cbc
Troels <trolund@gmail.com>
2025-02-13 20:09:51 +0100

    db test

--- src/LockNote.Data/CosmosDbService.cs
+++ src/LockNote.Data/CosmosDbService.cs
@@ -17,6 +17,7 @@ public class CosmosDbService(string? connectionString, CosmosDbSettings settings
 
     public async Task<Container> GetContainerAsync()
     {
+        return _cosmosClient.GetContainer(_databaseName, _containerName);
         var database = await _cosmosClient.CreateDatabaseIfNotExistsAsync(_databaseName, 4000,
             new RequestOptions() { PriorityLevel = PriorityLevel.High });
         return await database.Database.CreateContainerIfNotExistsAsync(_containerName, "/partitionKey", 4000);
