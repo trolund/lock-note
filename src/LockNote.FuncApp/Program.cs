7b60ad98d8b0268473d91eac556d9777034192a4
Troels <trolund@gmail.com>
2025-02-14 19:32:19 +0100

    Insights function app

--- src/LockNote.FuncApp/Program.cs
+++ src/LockNote.FuncApp/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -6,8 +8,8 @@ var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+ builder.Services
+     .AddApplicationInsightsTelemetryWorkerService()
+     .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
