7db4d954a15a75389429a255c09c4c293f776f68
Troels <trolund@gmail.com>
2025-02-14 20:06:30 +0100

    added http trigger

--- src/LockNote.FuncApp/CleanData.cs
+++ src/LockNote.FuncApp/CleanData.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
 namespace LockNote.FuncApp;
@@ -6,14 +8,26 @@ namespace LockNote.FuncApp;
 public class CleanData(ILogger<CleanData> logger)
 {
     [Function("CleanData")]
-    public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+    public void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
     {
-        logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        logger.LogInformation("C# Timer trigger function executed at: {DateTime.Now}");
+        
+        logger.LogWarning("This is a warning message");
 
         if (myTimer.ScheduleStatus is not null)
         {
-            logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            logger.LogInformation("Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             
         }
+    }
+    
+    // http trigger
+    [Function("CleanDataHttp")]
+    public HttpResponseData RunHttp([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString("Hello, world!");
+        return response;
     }
 }
